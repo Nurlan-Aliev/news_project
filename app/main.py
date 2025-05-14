5f5cf1018b137d96ee89c1c215213d5794bf9e68
Nurlan-Aliev <nualiev612@gmail.com>
2025-05-14 21:23:19 +0400

    add middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.auth.views import router as auth_router
 from app.news.ruoters import router as news_router
 from app.database import Base, db_helper
+from app.settings import settings
 
 
 @asynccontextmanager
@@ -13,6 +15,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ORIGIN,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(news_router, prefix="/news")
